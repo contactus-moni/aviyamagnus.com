076f497371187d5089e95e8a8dbff855d106c66f
Monika Anandan <contactus@aviyamagnus.com>
2025-10-21 17:06:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const PORT = process.env.PORT || 3000;
 
 // PostgreSQL database connection
 const pool = new Pool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_dpg-d3rm7q95pdvs73fql7s0-a,
   port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  user: process.env.DB_aviyamagnus,
+  password: process.env.DB_k6zXVRlotvtVRJzgRXKM0Z01CkQPz6dl,
+  database: process.env.DB_aviyamagnus,
   ssl: { rejectUnauthorized: false } // Required for Render Postgres
 });
 
