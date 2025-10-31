4dc0392a2586d125dc3d0b0af96dc10df46726f1
Monika Anandan <contactus@aviyamagnus.com>
2025-10-31 17:37:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,3 +28,6 @@ app.post("/register", (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Server running on port", port));
+
+app.post('/register', (req, res) => { ... });
+
