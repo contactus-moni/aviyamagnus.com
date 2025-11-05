d3fe188b247a5de576b705c94c817fd1c6ef435f
Monika Anandan <contactus@aviyamagnus.com>
2025-11-05 15:35:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import express from "express";
-import cors from "cors";
-import pkg from "pg";
-const { Pool } = pkg;
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
 
 const app = express();
 app.use(cors());
